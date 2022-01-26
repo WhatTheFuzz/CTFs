4348fe4b540642d88c3f9b6184a0baab80eb2e9e
Sean Deaton <sean.deaton@me.com>
2022-01-26 16:36:56 -0500

    Add compile line.

--- plaidCTF/kappa/hook.c
+++ plaidCTF/kappa/hook.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/* This is a CTF man, no one wants to sleep! */
+/* This is a CTF; no one wants to sleep!
+** Compile with:
+** gcc -shared -fpic -mtune=i386 -m32 -static ./hook.c -o hook.so
+*/
 
 unsigned int sleep(unsigned int seconds){
     puts("[+] We don't sleep around here.");
