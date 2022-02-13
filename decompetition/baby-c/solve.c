0f232d59ddc902a9e4088d6e097cba85a5cfe904
Sean Deaton <sean.deaton@me.com>
2022-02-13 09:25:21 -0500

    Add 100% solution for baby-c.

--- decompetition/baby-c/solve.c
+++ decompetition/baby-c/solve.c
@@ -2,37 +2,31 @@
 #include <ctype.h>
 #include <stdint.h>
 
-
 #define true 1
 #define false 0
 
 typedef unsigned char bool;
 
 int main(void){
-
-    uint32_t c;
+  
     bool first;
-
+    uint32_t c;
+    
     first = true;
     while(true){
         c = getc(stdin);
         if (c == -1)
             break;
-        if (!isspace(c)){
-            if (first){
-                c = toupper(c);
-                first = false;
-                putc(c, stdout);
-            }
-            else{
-                c = tolower(c);
-                putc(c, stdout);
-            }
-
+        if (isspace(c)){
+          putc(c, stdout);
+          first = true;
+        }
+        else if (first){
+          putc(toupper(c), stdout);
+          first = false;
         }
         else{
-            putc(c, stdout);
-            first = true;
+          putc(tolower(c), stdout);
         }
     }
     return 0;
