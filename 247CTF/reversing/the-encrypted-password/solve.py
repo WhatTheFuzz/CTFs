fdb2cd34271b4f57fbb8d34d2454c090cb95fa65
Sean Deaton <sean.deaton@me.com>
2022-03-03 15:58:42 -0500

    Bad attempt at a solution.

--- 247CTF/reversing/the-encrypted-password/solve.py
+++ 247CTF/reversing/the-encrypted-password/solve.py
@@ -4,12 +4,7 @@
 WhatTheFuzz's submission for the 247CTF challenge The Encrypted Password.
 
 This script can be used in the following manner:
-python3 ./solve.py <REMOTE/LOCAL>
-
-Args:
-    param1: LOCAL will operate locally on the user's machine.
-            REMOTE will connect to the CTF webserver and grab the flag.
-            If no parameter is specified, the program will default to LOCAL.
+python3 ./solve.py
 
 Returns:
     The flag to solve the challenge.
@@ -23,6 +18,11 @@ context.binary = exe
 context.log_level = 'info'
 context.terminal = ['gnome-terminal', '-e']
 
+GDB_SCRIPT = '''
+br strcmp@plt
+run
+'''
+
 def connection():
     '''Establish the connection to the process, local or remote.
     '''
@@ -35,6 +35,20 @@ def connection():
 
     return conn
 
+def get_password():
+    '''Return the password.
+    '''
+
+    with connection() as conn:
+
+        # Attach to the process.
+        pid, dbg = gdb.attach(conn, GDB_SCRIPT, api=True)
+
+        conn.wait(1)
+
+        conn.sendline('test')
+
+
 
 def main():
     '''Return the flag.
@@ -46,4 +60,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    get_password()
